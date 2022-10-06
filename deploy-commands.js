b4988a978d4f2cf6914d36d17bb57de2eaab7349
Joash Agesa <47209391+Jace254@users.noreply.github.com>
2022-10-06 09:29:08 +0300

    init

---
+++ deploy-commands.js
@@ -0,0 +1,23 @@
+import { REST, SlashCommandBuilder, Routes } from "discord.js";
+import { clientId, guildId, token } from "./constants.js";
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Replies with server info!"),
+  new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Replies with user info!"),
+].map((command) => command.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then((data) =>
+    console.log(`Successfully registered ${data.length} application commands.`)
+  )
+  .catch(console.error);
